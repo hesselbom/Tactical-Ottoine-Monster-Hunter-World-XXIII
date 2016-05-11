5cc6e4fea5a5ae1752751623218ecf553170e06d
Pierre Sköldborg <pierre.skoldborg@ottoboni.se>
2016-05-11 15:10:58 +0200

    Added some starter classes for player

---
+++ classes.js
@@ -0,0 +1,38 @@
+module.exports = [
+  {
+    name: 'Fighter',
+    base: {
+      attack: 70,
+      speed: 40,
+      health: 60,
+      mana: 30
+    }
+  },
+  {
+    name: 'Sorcerer',
+    base: {
+      attack: 30,
+      speed: 40,
+      health: 50,
+      mana: 80
+    }
+  },
+  {
+    name: 'Ranger',
+    base: {
+      attack: 40,
+      speed: 70,
+      health: 70,
+      mana: 20
+    }
+  },
+  {
+    name: 'Palladin',
+    base: {
+      attack: 50,
+      speed: 40,
+      health: 50,
+      mana: 60
+    }
+  }
+];
